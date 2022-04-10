a34b2e3c4f9e48f201ef6d792665b109b09ed6f7
DESKTOP-5EMPGQL\Esmanios <beoms42@gmail.com>
2022-04-10 18:34:53 +0900

    [조영인] + 카테고리 알고리즘 완료

--- app/src/main/java/com/example/dbdastrology/PerkResult.java
+++ app/src/main/java/com/example/dbdastrology/PerkResult.java
@@ -35,8 +35,20 @@ public class PerkResult extends AppCompatActivity {
         perks_3 = findViewById(R.id.perks_3);
         perks_4 = findViewById(R.id.perks_4);
 
+        String packName = this.getPackageName(); // 패키지명
+        int resID3h = getResources().getIdentifier(((ZombleAlive)getApplication()).state3h, "drawable", packName);
+        int resID6h = getResources().getIdentifier(((ZombleAlive)getApplication()).state6h, "drawable", packName);
+        int resID9h = getResources().getIdentifier(((ZombleAlive)getApplication()).state9h, "drawable", packName);
+        int resID12h = getResources().getIdentifier(((ZombleAlive)getApplication()).state12h, "drawable", packName);
+
+        perks_1.setImageResource(resID3h);
+        perks_2.setImageResource(resID6h);
+        perks_3.setImageResource(resID9h);
+        perks_4.setImageResource(resID12h);
+
         resultView = findViewById(R.id.textResult2);
 
+        String sql = "";
         int redCount = 0;
         int blueCount = 0;
         int purpleCount = 0;
@@ -62,9 +74,22 @@ public class PerkResult extends AppCompatActivity {
             if(categoryPerk.equals("green")) {
                 greenCount++;
             }
-
-            resultView.setText("RedCount : "+redCount+"\n blueCount :"+blueCount+"\n purpleCount :"+purpleCount+"\n greenCount :"+greenCount);
         }
+
+
+        if(blueCount >= 2 && redCount >= 2) {
+            sql += "정말 평범한 날이 될거야.";
+        } else if(blueCount >= 2)  {
+            sql += "최고의 날이야. \n 오늘은 너의 날이 될거야";
+        }else if(purpleCount >= 2)  {
+            sql += "이상한 날이야. \n 너와 관계없는 일이 주변에 생길거야";
+        } else if(redCount >= 2) {
+            sql += "좋은일보다는 나쁜일이 많아. \n 등뒤를 조심해";
+        } else  {
+            sql += "정말 무난한 날이야.";
+        }
+
+        resultView.setText(sql);
     }
 
 }
